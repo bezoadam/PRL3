2707f90c90d1e4e032286d1b830ae22dec57e5bf
Adam Bezak <bezoadam95@gmail.com>
2018-04-21 16:29:19 +0200

    vypis, komentare

--- pro.cpp
+++ pro.cpp
@@ -1,3 +1,8 @@
+/**
+	autor: Adam Bezak xbezak01@stud.fit.vutbr.cz
+	program: prirazeni poradi preorder vrcholum
+*/
+
 #include <mpi.h>
 #include <iostream>
 #include <fstream>
@@ -8,8 +13,12 @@
 
 using namespace std;
 
+// #define DEBUG 1
 #define TAG 0
 
+/**
+	Struktura hrany medzi dvoma vrcholmi
+*/
 struct Edge {
 	int number;
 	bool isForwardEdge;
@@ -17,31 +26,53 @@ struct Edge {
 	char end;
 };
 
+/**
+	Struktura sousednosti medzi dvoma vrcholmi
+*/
 struct AdjencyElement {
 	struct Edge edge;
 	struct Edge reverseEdge;
 };
 
+/**
+	Struktura adjency listu
+*/
 struct AdjChar {
 	char nodeChar;
 	vector<struct AdjencyElement> adjencies;
 };
 
+/**
+	Struktura pre pole suffix sum
+*/
 struct ProcEdge {
 	int myEdgeNumber;
 	int nextEdgeNumber;
+	int suffixSumValue;
 	bool isForwardEdge;
 	char start;
 	char end;
 };
 
+/**
+    Vypis struktury hrany.
+
+    @param struct Edge edge Hrana
+    @return void
+*/
 void printStructEdge(struct Edge edge) {
-	cout << "Number:" << edge.number << endl;
+	cout << "Number: " << edge.number << endl;
 	cout << "Is forward edge " << edge.isForwardEdge << endl;
-	cout << "Startovaci symbol: " << edge.start << endl;
-	cout << "Konecny symbol: " << edge.end << endl;
+	cout << "Start symbol: " << edge.start << endl;
+	cout << "End symbol: " << edge.end << endl;
 }
 
+/**
+    Vypis struktury adjency listu.
+
+    @param vector<struct AdjencyElement> adjencies adjency list
+    @return void
+*/
 void printVectorOfAdjencies(vector<struct AdjencyElement> adjencies) {
 	for(int i = 0; i < adjencies.size(); i++) {
 		cout << '\n';
@@ -53,6 +84,12 @@ void printVectorOfAdjencies(vector<struct AdjencyElement> adjencies) {
 	}
 }
 
+/**
+    Vypis struktury uzlov.
+
+    @param vector<struct AdjChar> characterNodes Jednotlive uzly.
+    @return void
+*/
 void printVectorCharacterNodes(vector<struct AdjChar> characterNodes) {
 	for(int i = 0; i < characterNodes.size(); i++) {
 		cout << '\n';
@@ -61,17 +98,31 @@ void printVectorCharacterNodes(vector<struct AdjChar> characterNodes) {
 	}
 }
 
-void printVector(vector<int> vec) {
-	for(int i = 0; i < vec.size(); i++)
-	{
-		if(vec[i] == -1) break;
-		cout << vec[i] << " ";
+/**
+    Vypis suffix sum vektora.
+
+    @param vector<struct ProcEdge> suffixSumResult Suffix sum vektor.
+    @return void
+*/
+void printFinalVector(vector <struct ProcEdge> suffixSumResult, string *output, int treeSize) {
+	for(int i =0; i < suffixSumResult.size(); i++) {
+		if (suffixSumResult[i].isForwardEdge) {
+			// cout << "My number: " << suffixSumResult[i].myEdgeNumber << " My suffix : " << treeSize - suffixSumResult[i].suffixSumValue << " start: " << suffixSumResult[i].start << " end " << suffixSumResult[i].end << endl << flush;
+			(*output)[treeSize - suffixSumResult[i].suffixSumValue] = suffixSumResult[i].end;
+		}
 	}
-	cout << endl;
 }
 
+/**
+    Zistenie nasledujucej hrany na zaklade aktualneho cisla hrany.
+
+    @param vector<struct AdjChar> characterNodes Jednotlive uzly.
+    @param int myEdgeNumber cislo zistovanej hrany
+    @return struct ProcEdge
+*/
 struct ProcEdge ETour(vector<struct AdjChar> characterNodes, int myEdgeNumber) {
 	struct ProcEdge procEdge = {
+		-1,
 		-1,
 		-1,
 		false,
@@ -82,6 +133,7 @@ struct ProcEdge ETour(vector<struct AdjChar> characterNodes, int myEdgeNumber) {
 		int leftSideIndex = 0;
 		for(int j = 0; j < characterNodes[i].adjencies.size(); j++) {
 			struct AdjencyElement element = characterNodes[i].adjencies[j];
+			// Postupne si ukladam reverznu hranu ako nasledujucu
 			if (element.edge.number == myEdgeNumber && procEdge.nextEdgeNumber == -1) {
 				procEdge.nextEdgeNumber = element.reverseEdge.number;
 				procEdge.isForwardEdge = element.edge.isForwardEdge;
@@ -89,11 +141,13 @@ struct ProcEdge ETour(vector<struct AdjChar> characterNodes, int myEdgeNumber) {
 				procEdge.end = element.edge.end;
 			}
 			if (element.reverseEdge.number == myEdgeNumber) {
+				// V pripade, ze hladam na konci adjency listu, tak berem ako dalsiu hranu z prveho prvku
 				if ((characterNodes[i].adjencies.size() - 1) == j) {
 					procEdge.nextEdgeNumber = characterNodes[i].adjencies[0].edge.number;
 					procEdge.start = element.reverseEdge.start;
 					procEdge.end = element.reverseEdge.end;
 				} else {
+					// V opacnom pripade si ulozim nasleduju hranu z nasledujuceho prvku adjency listu
 					procEdge.nextEdgeNumber = characterNodes[i].adjencies[j + 1].edge.number;
 					procEdge.start = element.reverseEdge.start;
 					procEdge.end = element.reverseEdge.end;
@@ -115,103 +169,113 @@ int main(int argc, char* argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);       // zistime pocet procesorov
 	MPI_Comm_rank(MPI_COMM_WORLD, &myId);           // zistime id svojho procesoru
 
-	// cout << tree << endl;
 	int counter = 1;
+	double startTime, endTime;
+	startTime = MPI_Wtime();	//pociatocny cas
 
-	for(int i = 0; i < tree.size(); i++) {
-		int current = i + 1;
-		struct AdjChar adjChar = {};
-		adjChar.nodeChar = tree[i];
+	if (myId == 0) {
+		//vytvorenie adjency listu
+		for(int i = 0; i < tree.size(); i++) {
+			int current = i + 1;
+			struct AdjChar adjChar = {};
+			adjChar.nodeChar = tree[i];
 
-		if ((2 * current) <= tree.size()) {
-			struct Edge edge = {
-				counter,
-				true,
-				tree[current - 1],
-				tree[2 * current - 1]
-			};
-			counter++;
+			// lavy syn
+			if ((2 * current) <= tree.size()) {
+				struct Edge edge = {
+					counter,
+					true,
+					tree[current - 1],
+					tree[2 * current - 1]
+				};
+				counter++;
 
-			struct Edge reverseEdge = {
-				counter,
-				false,
-				tree[2 * current - 1],
-				tree[current - 1]
-			};
-			counter++;
+				struct Edge reverseEdge = {
+					counter,
+					false,
+					tree[2 * current - 1],
+					tree[current - 1]
+				};
+				counter++;
 
-			struct AdjencyElement adjency = {
-				edge,
-				reverseEdge
-			};
+				struct AdjencyElement adjency = {
+					edge,
+					reverseEdge
+				};
 
-			adjChar.adjencies.push_back(adjency);
-		}
-
-		if ((2 * current + 1) <= tree.size()) {
-			struct Edge edge = {
-				counter,
-				true,
-				tree[current - 1],
-				tree[2 * current ]
-			};
-			counter++;
-
-			struct Edge reverseEdge = {
-				counter,
-				false,
-				tree[2 * current],
-				tree[current - 1]
-			};
-			counter++;
-
-			struct AdjencyElement adjency = {
-				edge,
-				reverseEdge
-			};
-			adjChar.adjencies.push_back(adjency);
-		}
-
-		if ((current / 2 > 0) && ((2 * current) <= tree.size())){
-			int index;
-			struct AdjencyElement adjency = {};
-			int nodesIndex = (i - 1) / 2;
-			if (current % 2 == 0) {
-				index = 0;
-				adjency.edge = characterNodes[nodesIndex].adjencies[index].reverseEdge;
-				adjency.reverseEdge = characterNodes[nodesIndex].adjencies[index].edge;
-			} else {
-				index = 1;
-				adjency.edge = characterNodes[nodesIndex].adjencies[index].reverseEdge;
-				adjency.reverseEdge = characterNodes[nodesIndex].adjencies[index].edge;
+				adjChar.adjencies.push_back(adjency);
 			}
-			adjency.reverseEdge.isForwardEdge = false;
-			adjChar.adjencies.push_back(adjency);				
+
+			//pravy syn
+			if ((2 * current + 1) <= tree.size()) {
+				struct Edge edge = {
+					counter,
+					true,
+					tree[current - 1],
+					tree[2 * current ]
+				};
+				counter++;
+
+				struct Edge reverseEdge = {
+					counter,
+					false,
+					tree[2 * current],
+					tree[current - 1]
+				};
+				counter++;
+
+				struct AdjencyElement adjency = {
+					edge,
+					reverseEdge
+				};
+				adjChar.adjencies.push_back(adjency);
+			}
+
+			//spetna hrana vracajuca sa na nadradeny uzel
+			if ((current / 2 > 0) && ((2 * current) <= tree.size())){
+				int index;
+				struct AdjencyElement adjency = {};
+				int nodesIndex = (i - 1) / 2;
+				if (current % 2 == 0) {
+					//lavy podstrom
+					index = 0;
+					adjency.edge = characterNodes[nodesIndex].adjencies[index].reverseEdge;
+					adjency.reverseEdge = characterNodes[nodesIndex].adjencies[index].edge;
+				} else {
+					//pravy podstrom
+					index = 1;
+					adjency.edge = characterNodes[nodesIndex].adjencies[index].reverseEdge;
+					adjency.reverseEdge = characterNodes[nodesIndex].adjencies[index].edge;
+				}
+				adjency.reverseEdge.isForwardEdge = false;
+				adjChar.adjencies.push_back(adjency);				
+			}
+			characterNodes.push_back(adjChar);
 		}
-		characterNodes.push_back(adjChar);
-		// cout << typeid(tree[i]).name() << endl;
 	}
 
-    const int nitems=5;
-    int          blocklengths[5] = {1,1,1,1,1};
-    MPI_Datatype types[5] = {MPI_INT, MPI_INT, MPI_INT, MPI_CHAR, MPI_CHAR};
+	// Vytvorenie MPI Datatypu struct ProcEdge
+    const int nitems=6;
+    int          blocklengths[nitems] = {1,1,1,1,1,1};
+    MPI_Datatype types[nitems] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT, MPI_CHAR, MPI_CHAR};
     MPI_Datatype mpi_edge_type;
-    MPI_Aint     offsets[5];
+    MPI_Aint     offsets[nitems];
 
     offsets[0] = offsetof(struct ProcEdge, myEdgeNumber);
     offsets[1] = offsetof(struct ProcEdge, nextEdgeNumber);
-	offsets[2] = offsetof(struct ProcEdge, isForwardEdge);
-    offsets[3] = offsetof(struct ProcEdge, start);
-    offsets[4] = offsetof(struct ProcEdge, end);
+    offsets[2] = offsetof(struct ProcEdge, suffixSumValue);
+	offsets[3] = offsetof(struct ProcEdge, isForwardEdge);
+    offsets[4] = offsetof(struct ProcEdge, start);
+    offsets[5] = offsetof(struct ProcEdge, end);
 
     MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_edge_type);
     MPI_Type_commit(&mpi_edge_type);
 
     int initNumber = 1;
     struct ProcEdge procEdge;
+
+    // Rozdistribujeme struktury ProcEdge jednotlivym procesorom podla funkcie ETour
 	if (myId == 0) {
-	    // cout << "numprocs " << numprocs << endl;
-		// printVectorCharacterNodes(characterNodes);
 		myEdgeNumber = myId + 1;
 
 		procEdge = ETour(characterNodes, myEdgeNumber);
@@ -221,67 +285,59 @@ int main(int argc, char* argv[])
 			struct ProcEdge procEdgeTmp;
 			procEdgeTmp = ETour(characterNodes, initNumber);
 			procEdgeTmp.myEdgeNumber = initNumber;
-			// cout << "My edge number: " << procEdgeTmp.myEdgeNumber << endl << "ETour next edge number: " << procEdgeTmp.nextEdgeNumber << endl << " my Id: " << myId + 1 << endl << endl;
 			MPI_Send(&procEdgeTmp, 1, mpi_edge_type, i, TAG, MPI_COMM_WORLD);
 			initNumber = procEdgeTmp.nextEdgeNumber;
 		}
 	}
 	MPI_Recv(&procEdge, 1, mpi_edge_type, 0, TAG, MPI_COMM_WORLD, &status);
-	// cout << myId << endl;
- 	MPI_Barrier(MPI_COMM_WORLD);
-	// cout << "My edge number: " << procEdge.myEdgeNumber << endl << "ETour next edge number: " << procEdge.nextEdgeNumber << endl << " my Id: " << myId + 1 << endl << endl;
 
-	int initialValue = 1;
-	if (myId == 0) {
-		MPI_Send(&initialValue, 1, MPI_INT, procEdge.nextEdgeNumber - 1, TAG, MPI_COMM_WORLD);
-	} else {
-		MPI_Recv(&initialValue, 1, MPI_INT, MPI_ANY_SOURCE, TAG, MPI_COMM_WORLD, &status);
-		initialValue++;
-		if (procEdge.nextEdgeNumber != 1) {
-			MPI_Send(&initialValue, 1, MPI_INT, procEdge.nextEdgeNumber - 1, TAG, MPI_COMM_WORLD);
-		}
-	}
+	// cout << "New value " << tree.size() - procEdge.suffixSumValue << "id proc " << myId + 1 << " id hrany " << procEdge.myEdgeNumber <<  " char: " << procEdge.end <<endl;
 
-	vector<int> suffixSum;
+ 	//suffix sum
 	int value = 0;
-	suffixSum.reserve(numprocs);
-
 	if (numprocs - 1 == myId) {
-		// cout << "My edge number: " << procEdge.myEdgeNumber << endl << "ETour next edge number: " << procEdge.nextEdgeNumber << endl << "Poradie: " << initialValue << endl << endl;
 		value = 0;
 	} else {
 		value = procEdge.isForwardEdge;
 	}
 	MPI_Barrier(MPI_COMM_WORLD);
 
-	// cout << "cpu: " << myId + 1 << " initial value " << initialValue << endl;
-	int newValue = 0;
+	procEdge.suffixSumValue = 0;
 	for (int i = numprocs - 1; i >= 0; i--) {
 		int receivedValue = 0;
-
 		if (myId == i) {
-			// cout << i << " id proc " << myId + 1 << endl;
 			if (myId == numprocs - 1) {
 				MPI_Send(&receivedValue, 1, MPI_INT, i - 1, TAG, MPI_COMM_WORLD);
-				// cout << receivedValue << endl;
 			} else if (i == 0) {
 				MPI_Recv(&receivedValue, 1, MPI_INT, i + 1, TAG, MPI_COMM_WORLD, &status);
-				newValue = value + receivedValue;
-				// cout << newValue << endl;	
+				procEdge.suffixSumValue = value + receivedValue;
 			} else {
-				// cout << "cakam" << endl;
 				MPI_Recv(&receivedValue, 1, MPI_INT, i + 1, TAG, MPI_COMM_WORLD, &status);
-				newValue = value + receivedValue;
-				// cout << "New valu " << newValue << " old " << value << " received " << receivedValue << " id proc " << myId + 1 << " id hrany " << procEdge.myEdgeNumber << " initiialValue " << initialValue << endl;
-				MPI_Send(&newValue, 1, MPI_INT, i - 1, TAG, MPI_COMM_WORLD);
+				procEdge.suffixSumValue = value + receivedValue;
+				MPI_Send(&procEdge.suffixSumValue, 1, MPI_INT, i - 1, TAG, MPI_COMM_WORLD);
 			}	
 		}
-	}
+	} 
 
-	MPI_Barrier(MPI_COMM_WORLD);
+	// Kazdy procesor odosle nultemu svoje poradie v suffix sum
+	MPI_Send(&procEdge, 1, mpi_edge_type, 0, TAG, MPI_COMM_WORLD);
+	if (myId == 0) {
+		// Nulty procesor prijme vsetky hodnoty suffix sum od procesorov a ulozi ich do vektoru
+		vector<struct ProcEdge> suffixSumResult;
+		suffixSumResult.resize(numprocs);
+		for (int i = 0; i < numprocs; i++) {
+			MPI_Recv(&suffixSumResult[i], 1, mpi_edge_type, i, TAG, MPI_COMM_WORLD, &status);
+		}
+		endTime = MPI_Wtime();
+		cerr << endTime-startTime;
 
-	if (procEdge.isForwardEdge) {
-		cout << "New valu " << newValue << "hrana c" << procEdge.myEdgeNumber << endl;
+		//Vypis preorder na zaklade suffix sum
+		string output;
+		output.resize(tree.size());
+		output[0] = procEdge.start;
+		printFinalVector(suffixSumResult, &output, tree.size());
+		cout << output;
+		cout << endl;
 	}
 
 	MPI_Finalize();
